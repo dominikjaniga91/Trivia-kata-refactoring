5d6831bd6fb9f2363aef32c9fead41024d3cfa27
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-02-23 15:49:58 +0100

    Delete useless isPlayable method

--- src/main/java/trivia/Game.java
+++ src/main/java/trivia/Game.java
@@ -31,11 +31,7 @@ public class Game implements IGame {
       return "Rock Question " + index;
    }
 
-   public boolean isPlayable() {
-      return (howManyPlayers() >= 2);
-   }
-
-   public boolean add(String playerName) {
+    public boolean add(String playerName) {
 
 
       players.add(playerName);
